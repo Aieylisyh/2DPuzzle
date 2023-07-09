b57758a1dad56ca2cd1785aa185b6d7681a8e589
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-07-09 19:25:20 +0800

    add 2 simple functions

---
+++ 2D Puzzle Example/Assets/Game/Scripts/game/CommonPuzzleUtil/ClickChangeImageQueue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ClickChangeImageQueue : MonoBehaviour
+{
+    public GameObject[] queue;
+    int crtIndex;
+
+    private void Start()
+    {
+        Set(0);
+    }
+
+    public void Next()
+    {
+        Set(crtIndex + 1);
+    }
+
+    public void Set(int index)
+    {
+        crtIndex = index;
+        var len = queue.Length;
+        var i = index % len;
+
+        for (int j = 0; j < len; j++)
+        {
+            queue[j].SetActive(j == i);
+        }
+    }
+
+    public void OnClick()
+    {
+        Next();
+    }
+}
