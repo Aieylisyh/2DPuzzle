1d87968aa81fe31a440e3348441960bd567708c8
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-07-15 12:16:42 +0800

    fix click feedback postion

--- 2D Puzzle Example/Assets/Game/Scripts/game/clickFeedback/ClickFeedbackBehaviour.cs
+++ 2D Puzzle Example/Assets/Game/Scripts/game/clickFeedback/ClickFeedbackBehaviour.cs
@@ -8,7 +8,10 @@ public class ClickFeedbackBehaviour : MonoBehaviour
 {
     public Image img;
     public Vector3 mousePos;
-
+    public float rx;
+    public float ry;
+    public float offsetX;
+    public float offsetY;
     void Start()
     {
         Destroy(gameObject, 1);
@@ -18,7 +21,16 @@ public class ClickFeedbackBehaviour : MonoBehaviour
         img.DOFade(0, 0.5f);
         img.raycastTarget = false;
 
-        GetComponent<RectTransform>().anchoredPosition3D = new Vector3(mousePos.x, mousePos.y, 0);
+        var p = Camera.main.ScreenToWorldPoint(mousePos);
+        Debug.Log(mousePos + " " + p);
+        var finalPos = new Vector2(p.x * rx + offsetX, p.y * ry + offsetY);
+        transform.position = finalPos;
+
+        var rect = GetComponent<RectTransform>();
+        var anchoredPos = rect.anchoredPosition3D;
+        anchoredPos.z = 0;
+        rect.anchoredPosition = anchoredPos;
+        //GetComponent<RectTransform>().anchoredPosition3D = new Vector3(finalPos.x, finalPos.y, 0);
 
         SoundSystem.instance.Play("click");
     }
