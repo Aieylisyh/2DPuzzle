34bf953834fe16a33f81312ab2619c75d3fe6978
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-05-01 13:15:25 +0800

    wip

--- 2D Puzzle Example/Assets/EndlessBook/Demos/Demo 03 - Manual Page Turn/Scripts/TintDemo.cs
+++ 2D Puzzle Example/Assets/EndlessBook/Demos/Demo 03 - Manual Page Turn/Scripts/TintDemo.cs
@@ -7,30 +7,17 @@ using UnityEngine.UI;
 public class TintDemo : MonoBehaviour
 {
     Color pickedColor;
-    public UnityEvent evt;
-    public UnityEvent<int> evt_int;
-    public UnityEvent<bool> evt_bool;
-    public UnityEvent<Color> evt_Color;
 
     public void Tint(GameObject g)
     {
         Image img = g.GetComponent<Image>();
-        img.color = pickedColor;
+        //img.color = pickedColor;
+        img.CrossFadeColor(pickedColor, 0.5f, false, false);
     }
 
     public void PickColor(GameObject g)
     {
         Image img = g.GetComponent<Image>();
         pickedColor = img.color;
-    }
-
-    public void TestColor(Color c)
-    {
-        List<GameObject> list;
-    }
-
-    public void TestBool(bool b)
-    {
-
     }
 }
