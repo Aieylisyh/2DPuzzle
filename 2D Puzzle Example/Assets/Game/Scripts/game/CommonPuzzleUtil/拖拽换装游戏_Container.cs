d6add6fb45257ff7e8e9bc00e82f92ac9d6cad8a
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-07-29 13:01:07 +0800

    wip

---
+++ 2D Puzzle Example/Assets/Game/Scripts/game/CommonPuzzleUtil/拖拽换装游戏_Container.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class 拖拽换装游戏_Container : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, ICancelHandler
+{
+    [HideInInspector]
+    public bool inside;
+    [HideInInspector]
+    public RectTransform rectTrans;
+
+    [System.Serializable]
+    public class 拖拽换装游戏_Ref
+    {
+        public 拖拽换装游戏_Target.拖拽换装游戏_SubType type;
+        public RectTransform rt;
+        public 拖拽换装游戏_Target currentTarget;
+    }
+
+
+    public 拖拽换装游戏_Ref[] refs;
+
+    private void Awake()
+    {
+        rectTrans = GetComponent<RectTransform>();
+
+    }
+
+    public 拖拽换装游戏_Ref GetRef(拖拽换装游戏_Target tb)
+    {
+        foreach (var r in refs)
+            if (r.type == tb.subType) return r;
+        return null;
+    }
+
+    public void ReceiveTarget(拖拽换装游戏_Target tb, bool swap)
+    {
+        if (refs == null)
+            return;
+
+        //  Debug.Log(gameObject.name + " Container ReceiveTarget " + tb.gameObject.name);
+
+        tb.rectTrans.SetParent(transform);
+        var reference = GetRef(tb);
+        var lastDDC = tb.lastDDC;
+        // Debug.Log(lastDDC.gameObject.name);
+        tb.lastDDC = this;
+        var lastTarget = reference.currentTarget;
+        reference.currentTarget = tb;
+        tb.rectTrans.anchoredPosition = reference.rt.anchoredPosition;
+
+        if (swap && reference.currentTarget != null)
+        {
+            //    Debug.Log("swap");
+            lastTarget.SetToDragDropContainer(lastDDC, false);
+        }
+    }
+
+    public void OnCancel(BaseEventData eventData)
+    {
+        inside = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerEnter " + this);
+        inside = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        inside = false;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+}
---
+++ 2D Puzzle Example/Assets/Game/Scripts/game/CommonPuzzleUtil/拖拽换装游戏_Target.cs
@@ -0,0 +1,81 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class 拖拽换装游戏_Target : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public enum 拖拽换装游戏_SubType
+    {
+        Cloth,
+        Shoe,
+        Hair,
+        Pant,
+    }
+
+    public 拖拽换装游戏_SubType subType;
+
+    private Canvas _canvas;
+
+    [HideInInspector]
+    public RectTransform rectTrans;
+
+    [SerializeField]
+    RectTransform draggingParent;
+
+    public 拖拽换装游戏_Container[] containers;
+    [SerializeField]
+    拖拽换装游戏_Container _startDDC;
+
+    [HideInInspector]
+    public 拖拽换装游戏_Container lastDDC;
+
+    private void Awake()
+    {
+        rectTrans = GetComponent<RectTransform>();
+        _canvas = GetComponentInParent<Canvas>();
+
+        lastDDC = _startDDC;
+        SetToDragDropContainer(_startDDC, false);
+    }
+
+    public void SetToDragDropContainer(拖拽换装游戏_Container ddc, bool swap = true)
+    {
+        if (ddc == null)
+            return;
+
+        ddc.ReceiveTarget(this, swap);
+        GetComponent<Image>().raycastTarget = true;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GetComponent<Image>().raycastTarget = false;
+        rectTrans.SetParent(draggingParent.transform);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTrans.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        拖拽换装游戏_Container endContainer = null;
+        foreach (var ddc in containers)
+        {
+            if (ddc.inside)
+            {
+                endContainer = ddc;
+                break;
+            }
+        }
+
+        SetToDragDropContainer(endContainer == null ? lastDDC : endContainer);
+
+        foreach (var ddc in containers)
+        {
+            if (ddc != endContainer)
+                ddc.inside = false;
+        }
+    }
+}
