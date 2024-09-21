18c4c29d61da9f8182099ea00829b61e97a050dc
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-09-21 10:43:21 +0800

    wip

---
+++ 2D Puzzle Example/Assets/Game/Scripts/game/Recommendation letter/RLSystem2.cs
@@ -0,0 +1,52 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public partial class RLSystem : MonoBehaviour
+{
+    [SerializeField] Image _blackScreen;
+
+    [SerializeField] CanvasGroup desk;
+
+    [SerializeField] CanvasGroup calendar;
+
+    // [SerializeField] 
+
+    [SerializeField] CanvasGroup checkedList;
+
+    [SerializeField] CanvasGroup map;
+
+    public void OnClickPaperOnDesk()
+    {
+
+    }
+
+    public void OnCalender()
+    {
+
+    }
+
+    public void OnClickExitCalender()
+    {
+        //Show check list
+    }
+
+    public void OnClickChecklistItem()
+    {
+        //check the 1st item
+        //todo add some words
+    }
+
+    public void OnClickExitChecklist()
+    {
+        //black screen walk sound
+        //show map to choose go where
+    }
+
+    public void OnClickDestination()
+    {
+
+    }
+}
