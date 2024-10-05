9187685b63e598c8a9baeb3992a4bb95ad6993af
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-10-05 10:30:58 +0800

    wip

---
+++ 2D Puzzle Example/Assets/Game/Scripts/com/Gameobject/Effect/Sprite/UiImageScreenFader.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com
+{
+    public class UiImageScreenFader : MonoBehaviour
+    {
+        public Image blackScreen;
+        public Image whiteScreen;
+        public static UiImageScreenFader instance;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public void FadeBlack(Action cb, float inTime = 0.5f, float outTime = 0.5f, float delay = 0.2f)
+        {
+            Fade(blackScreen, cb, inTime, outTime, delay);
+        }
+
+        public void FadeWhite(Action cb, float inTime = 0.5f, float outTime = 0.5f, float delay = 0.2f)
+        {
+            Fade(whiteScreen, cb, inTime, outTime, delay);
+        }
+
+        public void Fade(Image img, Action cb, float inTime = 0.5f, float outTime = 0.5f, float delay = 0.2f)
+        {
+            img.DOKill();
+            img.DOFade(1, inTime).OnComplete(() =>
+            {
+                cb?.Invoke();
+                img.DOFade(0, outTime).SetDelay(delay);
+            });
+        }
+    }
+}
---
+++ 2D Puzzle Example/Assets/Game/Scripts/game/Recommendation letter/ScreenEffectToggle.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScreenEffectToggle : MonoBehaviour
+{
+    public static ScreenEffectToggle instance;
+    public GameObject[] realityVfxs;
+    public GameObject[] dreamVfxs;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void ToggleDreamReality(bool b)
+    {
+        foreach (var v in realityVfxs)
+        {
+            v.SetActive(!b);
+        }
+        foreach (var v in dreamVfxs)
+        {
+            v.SetActive(b);
+        }
+    }
+}
