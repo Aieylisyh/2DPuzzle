555487db60ee1b306c6aea6b2a96e0f9c8f47705
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-10-09 00:42:08 +0800

    wip

--- 2D Puzzle Example/Assets/Game/Scripts/game/RescueGame/RescueSystem_7.cs
+++ 2D Puzzle Example/Assets/Game/Scripts/game/RescueGame/RescueSystem_7.cs
@@ -13,7 +13,17 @@ namespace Rescue
 
         IEnumerator StartScene_ClockNarrative(float delay)
         {
+            var cg = boyClock.GetComponent<CanvasGroup>();
+            cg.blocksRaycasts = true;
+            cg.interactable = true;
+            cg.alpha = 0;
+            foreach (var bg in boyClockBeforeActiveGroup)
+                bg.SetActive(true);
+            foreach (var bg in boyClockAfterActiveGroup)
+                bg.SetActive(false);
+
             yield return new WaitForSeconds(delay);
+            cg.DOFade(1, 1);
         }
 
         public void OnPuzzleEnd_ClockNarrative()
@@ -24,6 +34,39 @@ namespace Rescue
         public void StartPuzzle_ClockNarrative()
         {
             ToggleCg(sceneCg_ClockNarrative, false);
+        }
+
+        [SerializeField] ClockBehaviour boyClock;
+        [SerializeField] ClockBehaviour girlClock;
+        [SerializeField] GameObject[] boyClockBeforeActiveGroup;
+        [SerializeField] GameObject[] boyClockAfterActiveGroup;
+        [SerializeField] RectTransform boyPoolDragTarget;
+        [SerializeField] float boyPoolDragDistanceThreshold;
+
+        public void ReachBoyClockEnd()
+        {
+            boyClock.enabled = false;
+            var cg = boyClock.GetComponent<CanvasGroup>();
+            cg.blocksRaycasts = false;
+            cg.interactable = false;
+            cg.DOFade(0, 2).OnComplete(ShowDragBoyToPool);
+        }
+
+        void ShowDragBoyToPool()
+        {
+            foreach (var bg in boyClockBeforeActiveGroup)
+                bg.SetActive(false);
+            foreach (var bg in boyClockAfterActiveGroup)
+                bg.SetActive(true);
+        }
+
+        public void ReachGirlClockEnd()
+        {
+            girlClock.enabled = false;
+            var cg = girlClock.GetComponent<CanvasGroup>();
+            cg.blocksRaycasts = false;
+            cg.interactable = false;
+            cg.DOFade(0, 2);
         }
     }
 }
