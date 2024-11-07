016a71037a15f029ea439f2282f3f39e89c9199c
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-11-07 16:46:23 +0800

    wip

--- 2D Puzzle Example/Assets/Game/Scripts/game/LondonLifeGame/MeetGirlAndLaterScenes/自己做饭System.cs
+++ 2D Puzzle Example/Assets/Game/Scripts/game/LondonLifeGame/MeetGirlAndLaterScenes/自己做饭System.cs
@@ -34,4 +34,94 @@ public class 自己做饭System : MonoBehaviour
     {
         StartCoroutine(EndScene());
     }
+
+    [SerializeField] Image Carrot;
+    [SerializeField] Image Potato;
+    [SerializeField] Image Chicken;
+
+    [SerializeField] Image Carrot_sliced;
+    [SerializeField] Image Potato_sliced;
+    [SerializeField] Image Chicken_sliced;
+
+    [SerializeField] Image Knife;
+    [SerializeField] Image Ladle;
+    [SerializeField] Image Kettle;
+    [SerializeField] Image EmptyPot;
+    [SerializeField] Image CurryPot;
+
+    [SerializeField] GameObject cuttingKnife;
+    [SerializeField] GameObject mixingLadle;
+    [SerializeField] GameObject pouringKettle;
+
+    bool _isSelectedKnife;
+    bool _isSelectedLadle;
+
+    void ShowCutAnimation(Image toHide, Image imageToShow)
+    {
+        Knife.enabled = false;
+        toHide.DOFade(0, 1).SetDelay(2).OnComplete(
+            () =>
+            {
+                Knife.enabled = true;
+            });
+
+    }
+    public void OnClick_Carrot()
+    {
+        if (_isSelectedKnife)
+        {
+            ShowCutAnimation(Carrot, Carrot_sliced);
+
+        }
+    }
+
+    public void OnClick_Potato()
+    {
+
+    }
+
+    public void OnClick_Chicken()
+    {
+
+    }
+
+    public void OnClick_Carrot_sliced()
+    {
+
+    }
+
+    public void OnClick_Potato_sliced()
+    {
+
+    }
+
+    public void OnClick_Chicken_sliced()
+    {
+
+    }
+
+    public void OnClick_Kettle()
+    {
+
+    }
+
+    public void OnClick_Ladle()
+    {
+
+    }
+
+    public void OnClick_Knife()
+    {
+
+    }
+
+    public void OnClick_EmptyPot()
+    {
+
+    }
+
+    public void OnClick_CurryPot()
+    {
+
+    }
 }
