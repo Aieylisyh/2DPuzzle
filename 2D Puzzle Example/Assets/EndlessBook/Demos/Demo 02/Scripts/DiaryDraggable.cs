7b406637ee105dc980cae2ee861ffc530719c1ee
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-11-25 00:51:19 +0800

    wip

---
+++ 2D Puzzle Example/Assets/EndlessBook/Demos/Demo 02/Scripts/DiaryDraggable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.EndlessBook.Demos.Demo_02.Scripts
+{
+    public class DiaryDraggable : MonoBehaviour
+    {
+        public float dragFactor = 100;
+        bool _dragging;
+        Vector3 _posStartDrag;
+
+        public void StartDrag()
+        {
+            _dragging = true;
+            _posStartDrag = transform.position;
+        }
+
+        public void EndDrag()
+        {
+            _dragging = false;
+        }
+
+        public void OnDrag(Vector2 increment)
+        {
+            if (_dragging)
+            {
+                _posStartDrag += (Vector3)increment * dragFactor;
+                transform.position = _posStartDrag;
+            }
+        }
+    }
+}
