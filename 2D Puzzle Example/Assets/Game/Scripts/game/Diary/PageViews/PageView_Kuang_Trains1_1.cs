ab2f29b89e3c6c7ad34f79bb6c0d2ffab3cbdd90
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-12-19 18:39:13 +0800

    wip

--- 2D Puzzle Example/Assets/Game/Scripts/game/Diary/PageViews/PageView_Kuang_Trains1_1.cs
+++ 2D Puzzle Example/Assets/Game/Scripts/game/Diary/PageViews/PageView_Kuang_Trains1_1.cs
@@ -61,7 +61,7 @@ public class PageView_Kuang_Trains1_1 : PageView
             return;
         if (friendTalksDone)
             return;
-        SoundSystem.instance.Play("done");
+        SoundSystem.instance.Play("s2");
         tinyStage.StartPlay(ShowFriendTalkLeft);
     }
 
